d2cd8b2dec568b60469ffb01ef1ddd5ac63ddd67
OmegaSunkey <bronytecfan@gmail.com>
2022-06-28 10:53:37 -0500

    idkkkkk

--- UserPFP/src/main/kotlin/om/ega/sunkey/userpfp/model/USRPFP.kt
+++ UserPFP/src/main/kotlin/om/ega/sunkey/userpfp/model/USRPFP.kt
@@ -26,6 +26,8 @@ object USRPFP : AbstractDatabase() {
     override val mapCache: MutableMap<Long, PFP> = HashMap()
     override val name: String = "UserPFP"
 
+	val t = Thread()
+
     override fun runPatches(patcher: PatcherAPI, settings: SettingsAPI) {
         patcher.patch(
             IconUtils::class.java.getDeclaredMethod(
@@ -36,11 +38,11 @@ object USRPFP : AbstractDatabase() {
                 Boolean::class.javaPrimitiveType,
                 Integer::class.java
             ), Hook {
-		Utils.threadPool.execute {
+		t() {
                 if (it.result.toString().contains(".gif") && settings.getBool(
                         "nitroBanner",
                         true
-                    ))  return@execute
+                    ))  return@t
                 val id = it.args[0] as Long
                 if (mapCache.containsKey(id)) {
                     it.result = mapCache[id]?.let { it1 ->  if ((it.args[3] as Boolean)) it1.animated else it1.static
@@ -64,8 +66,8 @@ object USRPFP : AbstractDatabase() {
 
         patcher.patch(
             IconUtils::class.java.getDeclaredMethod("setIcon", ImageView::class.java, String::class.java, Int::class.javaPrimitiveType, Int::class.javaPrimitiveType, Boolean::class.javaPrimitiveType, Function1::class.java, MGImages.ChangeDetector::class.java), Hook {
-	     Utils.threadPool.execute {
-                if ((it.args[1] as String).contains("https://cdn.discordapp.com/role-icons")) return@execute
+	     t() {
+                if ((it.args[1] as String).contains("https://cdn.discordapp.com/role-icons")) return@t
 
                 val simpleDraweeView = it.args[0] as SimpleDraweeView
                 simpleDraweeView.apply {
