17ba4e25b1b9bafdc3276ea69e142c4f126afb94
OmegaSunkey <bronytecfan@gmail.com>
2022-06-28 11:28:53 -0500

    this is prerelease i will fix freezing issues later

--- UserPFP/src/main/kotlin/om/ega/sunkey/userpfp/model/USRPFP.kt
+++ UserPFP/src/main/kotlin/om/ega/sunkey/userpfp/model/USRPFP.kt
@@ -36,11 +36,11 @@ object USRPFP : AbstractDatabase() {
                 Boolean::class.javaPrimitiveType,
                 Integer::class.java
             ), Hook {
-		Thread {
+		
                 if (it.result.toString().contains(".gif") && settings.getBool(
                         "nitroBanner",
                         true
-                    ))  return@Thread
+                    ))  return@Hook
                 val id = it.args[0] as Long
                 if (mapCache.containsKey(id)) {
                     it.result = mapCache[id]?.let { it1 ->  if ((it.args[3] as Boolean)) it1.animated else it1.static
@@ -58,14 +58,14 @@ object USRPFP : AbstractDatabase() {
                     
 		   }
                  }
-	       }
+	       
             }
         )
 
         patcher.patch(
             IconUtils::class.java.getDeclaredMethod("setIcon", ImageView::class.java, String::class.java, Int::class.javaPrimitiveType, Int::class.javaPrimitiveType, Boolean::class.javaPrimitiveType, Function1::class.java, MGImages.ChangeDetector::class.java), Hook {
-	     Thread {
-                if ((it.args[1] as String).contains("https://cdn.discordapp.com/role-icons")) return@Thread
+	     
+                if ((it.args[1] as String).contains("https://cdn.discordapp.com/role-icons")) return@Hook
 
                 val simpleDraweeView = it.args[0] as SimpleDraweeView
                 simpleDraweeView.apply {
@@ -74,7 +74,7 @@ object USRPFP : AbstractDatabase() {
                     background =
                         ShapeDrawable(OvalShape()).apply { paint.color = Color.TRANSPARENT }
                 }
-		}
+		
             })
     }
 
