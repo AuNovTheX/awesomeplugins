a46c48a114fecf0255532f7f242df62b21739a91
OmegaSunkey <bronytecfan@gmail.com>
2022-07-26 11:06:35 -0500

    java util

---
+++ Booru/src/main/kotlin/om/ega/sunkey/Booru/util/BAIS.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.io.*;
+
+public class BAIS {
+	public static InputStream guh(String h) {
+		var bair = new ByteArrayInputStream(h.getBytes());
+		return bair
+	}
+
+}
